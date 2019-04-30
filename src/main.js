c1ed52c34c3ab121125cd057ad5f34846b32158a
craill <craill@163.com>
2019-04-30 10:47:17 +0800

    公共样式-头部和尾部完成

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入css样式,如果不用这个变量,可以省略xxx from
+// import maincss from './assets/statics/site/css/style.css'
+import './assets/statics/site/css/style.css'
 
 Vue.config.productionTip = false
 
